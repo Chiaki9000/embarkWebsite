6b51d05bb20b3df05051a69af1e1faf5076641dd
Chiaki9000 <kevinlihaoxuan@gmail.com>
2024-04-07 19:52:59 -0700

    spacing

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wait for the DOM to fully load
 document.addEventListener('DOMContentLoaded', function () {
-    // Add event listener for each "More Info" button
     document.querySelectorAll('.overlay-button').forEach(button => {
         button.addEventListener('click', function () {
             var overlayId = this.id.replace('-overlay-btn', '-overlay');
@@ -9,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Add event listener for clicking on an overlay to hide it
     document.querySelectorAll('.overlay').forEach(overlay => {
         overlay.addEventListener('click', function () {
             this.style.display = 'none';
